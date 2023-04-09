345d793eaad02a84eaa444b5b17e9dc4c7cca9d5
yo1956 <hm.hr.yossy@gmail.com>
2023-04-09 22:56:58 +0900

    working on problem 36

--- 00_Debug/debug.py
+++ 00_Debug/debug.py
@@ -1,13 +1,42 @@
 from typing import Dict, List
-from collections import defaultdict
+#from collections import defaultdict
+import collections
 #from typing import list
 
 class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set)  # key = (r /3, c /3)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (
+                    board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]
+                ):
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+
+        return True
 
 def main():
     sol = Solution()
-    nums = [1,2,3,4]
-    ans = sol.productExceptSelf(nums)
+    board = [["8","3",".",".","7",".",".",".","."]
+            ,["6",".",".","1","9","5",".",".","."]
+            ,[".","9","8",".",".",".",".","6","."]
+            ,["8",".",".",".","6",".",".",".","3"]
+            ,["4",".",".","8",".","3",".",".","1"]
+            ,["7",".",".",".","2",".",".",".","6"]
+            ,[".","6",".",".",".",".","2","8","."]
+            ,[".",".",".","4","1","9",".",".","5"]
+            ,[".",".",".",".","8",".",".","7","9"]]
+    ans = sol.isValidSudoku(board)
     print(ans)
     
 if __name__ == "__main__":
