5418e38ac137c418e279f826f2b10742ed6638a0
yo1956 <hm.hr.yossy@gmail.com>
2023-04-10 19:37:05 +0900

    solved problem 36

--- 00_Debug/debug.py
+++ 00_Debug/debug.py
@@ -5,24 +5,29 @@ import collections
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        cols = collections.defaultdict(set)
-        rows = collections.defaultdict(set)
-        squares = collections.defaultdict(set)  # key = (r /3, c /3)
-
+        #rows = collections.defaultdict(set)
+        #cols = collections.defaultdict(set)
+        #square = collections.defaultdict(set)
+        
+        rows = dict(set)
+        cols = dict(set)
+        square = dict(set)
+        
         for r in range(9):
             for c in range(9):
-                if board[r][c] == ".":
+                if board[r][c] == '.':
                     continue
-                if (
-                    board[r][c] in rows[r]
+                
+                if(    board[r][c] in rows[r]
                     or board[r][c] in cols[c]
-                    or board[r][c] in squares[(r // 3, c // 3)]
-                ):
+                    or board[r][c] in square[(r//3, c//3)]
+                  ):
                     return False
-                cols[c].add(board[r][c])
+                
                 rows[r].add(board[r][c])
-                squares[(r // 3, c // 3)].add(board[r][c])
-
+                cols[c].add(board[r][c])
+                square[(r//3, c//3)].add(board[r][c])
+        
         return True
 
 def main():
--- 01_Arrays_and_Hashing/36.valid-sudoku_neetcode.py
+++ 01_Arrays_and_Hashing/36.valid-sudoku_neetcode.py
@@ -9,25 +9,24 @@
 import collections 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = collections.defaultdict(set)
         cols = collections.defaultdict(set)
-        square = collections.defaultdict(set)
-        
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set)  # key = (r /3, c /3)
+
         for r in range(9):
             for c in range(9):
-                if board[r][c] == '.':
+                if board[r][c] == ".":
                     continue
-                
-                if(    board[r][c] in rows[r]
+                if (
+                    board[r][c] in rows[r]
                     or board[r][c] in cols[c]
-                    or board[r][c] in square[(r//3, c//3)]
-                  ):
+                    or board[r][c] in squares[(r // 3, c // 3)]
+                ):
                     return False
-                
-                rows[r].add(board[r][c])
                 cols[c].add(board[r][c])
-                square[(r//3, c//3)].add(board[r][c])
-        
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+
         return True
         
 # @lc code=end
